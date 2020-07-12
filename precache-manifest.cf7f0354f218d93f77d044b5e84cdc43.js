3cba639dd970262a1245dace044dd4aacccc1a7a
Thami Memel <811998aZ->
2020-07-12 17:27:57 +0100

    Updates

---
+++ precache-manifest.cf7f0354f218d93f77d044b5e84cdc43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c451c6a4a1711f6fb52fd22c74ffb65",
+    "url": "/random-quotes-machine/index.html"
+  },
+  {
+    "revision": "6800c907603086457773",
+    "url": "/random-quotes-machine/static/css/2.6e45efcc.chunk.css"
+  },
+  {
+    "revision": "65c8333b2895796d0052",
+    "url": "/random-quotes-machine/static/css/main.325ad1fc.chunk.css"
+  },
+  {
+    "revision": "6800c907603086457773",
+    "url": "/random-quotes-machine/static/js/2.b8444f31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quotes-machine/static/js/2.b8444f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c8333b2895796d0052",
+    "url": "/random-quotes-machine/static/js/main.2200db4b.chunk.js"
+  },
+  {
+    "revision": "bbf41f3853eda730ccdf",
+    "url": "/random-quotes-machine/static/js/runtime-main.74c7c47f.js"
+  }
+]);
